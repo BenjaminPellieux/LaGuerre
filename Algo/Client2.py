f960552900219c83a218eaf9022b78d622ffff9d
BenjaminPellieux <hypophyse8@gmail.com>
2022-01-22 00:46:51 +0100

    Fonctionalite reseau fonctionnel

--- Algo/Client2.py
+++ Algo/Client2.py
@@ -10,11 +10,11 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((host, port))
 print(f"Connection on {port}")
 socket.send("JOIN|EKIP".encode())
-
 while True:
     msg_serv=socket.recv(255).decode()
     print(f"RECU: {msg_serv=}  ")
     if msg_serv: 
-        socket.send(build_message("MOVE", [0,2,"E"]))
+        socket.send(build_message("MOVE", [0,2, "N"]))
+
 print("Close")
 socket.close()
--- Algo/Interface.py
+++ Algo/Interface.py
@@ -1,8 +1,10 @@
+from distutils import command
+from msilib.schema import Error
 import pygame, sys, string
 from pygame.locals import *
 from random import randint
 
-NBJOUEUR=3
+NBJOUEUR=2
 NBCASES=5
 HEIGHT=1000
 BASE_UNITE_SIZE=12
@@ -85,7 +87,7 @@ class Game():
 
         self.serveur=Serveur()
         self.serveur.getJoueurs()
-        self.joueurs=[Joueur(i,f"Player {i+1}",self.positionJoueur[i],self.colorJoueur[i],self.nbcases) for i in range(len(self.serveur.players))]
+        self.joueurs=[Joueur(i,self.serveur.teamName[i],self.positionJoueur[i],self.colorJoueur[i],self.nbcases) for i in range(len(self.serveur.players))]
         self.serveur.communication()
 
         self.cases=[[None for j in range(self.nbcases)] for i in range(self.nbcases)]
@@ -106,17 +108,19 @@ class Game():
                         if self.cases[voisin[0]][voisin[1]]:
                             thecase.liens.add(Lien(thecase,self.cases[voisin[0]][voisin[1]]))
         
-    def moveUnite(self,idJoueur:int,parsize:int,idUnite:int,direction:string):
+    def moveUnite(self,idJoueur:int,params:list):
         #unite=self.joueurs[idJoueur].listUnite[posfrom[0]][posfrom[1]]
+        idUnite,parsize,direction=params
+        idUnite,parsize=int(idUnite),int(parsize)
         unite=self.joueurs[idJoueur].getUniteById(idUnite)
 
         #getUniteByPos(posfrom)
-        if unite:   
+        if unite and direction!="M":   
             ##MOUVEMENT
             posfrom=(unite.posx,unite.posy)
             newpos=self.getNewPos(posfrom,direction)
             if self.verifierLien(posfrom,newpos):   
-                if parsize<=unite.size:
+                if parsize<unite.size or not (unite.size-parsize): 
                     newpos_unite=self.joueurs[idJoueur].listUnite[newpos[0]][newpos[1]]
                     #getUniteByPos(newpos)
                     unite.size-=parsize
@@ -126,10 +130,14 @@ class Game():
                         self.joueurs[idJoueur].nbUnite+=1
                         newUnite=Unite(self.joueurs[idJoueur],self.joueurs[idJoueur].nbUnite,newpos,parsize)
                         self.joueurs[idJoueur].listUnite[newpos[0]][newpos[1]]=newUnite
+                elif parsize==unite.size:
+                    self.joueurs[idJoueur].listUnite[unite.posx][unite.posy]=None
+                    self.joueurs[idJoueur].listUnite[newpos[0]][newpos[1]]=unite
+                    unite.posx,unite.posy=newpos
 
-                else: print("ERROR: PAS ASSEZ DE SOLDAT")
-            else: print("ERROR: LIEN INUTILISABLE") 
-        else: print("ERROR: PAS D'UNITE A CETTE POSITION")
+                else: print(f"{idJoueur} ERROR: PAS ASSEZ DE SOLDAT")
+            else: print(f" {idJoueur} ERROR: LIEN INUTILISABLE") 
+        else: print(f"{idJoueur} ERROR: PAS D'UNITE A CETTE POSITION")
 
 
     def actualise(self):
@@ -192,7 +200,7 @@ class Game():
             print("ERROR")
         return (depart[0],depart[1])
 
-
+                    
     def verifierLien(self,depart:tuple,newpos:tuple)->bool:
         for lien in self.cases[depart[0]][depart[1]].liens:
             if (lien.toCase.posx,lien.toCase.posy)==newpos:
@@ -207,6 +215,7 @@ class Interface(Game):
 
         pygame.init()        
         self.pause=False 
+
         self.run()
 
 
@@ -217,16 +226,25 @@ class Interface(Game):
         self.font=pygame.font.Font(None, 50)
         self.font_unite=pygame.font.Font(None, int(self.size_case*0.8))
         while True:
-            
             if not self.pause:
                 self.affchageDamier()
                 self.affichageJoueur()
 
             ##########TEST#################
-            self.TEST.test(nb_tour)
+            #self.TEST.test(nb_tour)
             ##########TEST#################
-
+            try:
+                    
+                commandJoueur=self.serveur.communication()
+                for key in commandJoueur:
+                    if commandJoueur[key][0]=="MOVE":
+                        self.moveUnite(key,commandJoueur[key][1])
+            except Exception as e:
+                print(f"ERROR: {e} ")    
             self.actualise()
+
+
+
 
             for event in pygame.event.get():
                 if event.type==QUIT:
@@ -341,12 +359,12 @@ class Serveur():
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(('', 5000))
-
+        self.teamName=[]
     # Attente de la connexion des joueurs
         self.players = []
 
     def getJoueurs(self):
-        print("DEBUG: WAITING FOR PLAYER")
+        print("[INFO] WAITING FOR PLAYER")
         while len(self.players) < self.maxPlayer:
             self.socket.listen(5)
             client, address = self.socket.accept()
@@ -364,37 +382,36 @@ class Serveur():
 
             print(f"[INFO] Nouveau client accepté avec le nom {params[0]}")
             self.players.append(client)
+            self.teamName.append(params[0])
+
 
             # Annonce du début de la partie
         print("[INFO] Tous les joueurs ont rejoins, début de la partie")
         for i, player in enumerate(self.players):
             player.send(self.build_message("NEWGAME", [5, self.maxPlayer, i, 0, 0]))
-        
 
     def communication(self):
         print("DEBUG: WAITING FOR PLAYER MOVE ")
         # Tant que la game tourne
         commandeJoueur={} 
-        while True:
-            for player_id, player in enumerate(self.players):
-                player.send(self.build_message("NEWTURN", [0]))
-                # Envoie de tous les nouveaux événements
-                move = player.recv(255).decode()
-                
-                # Si cela arrive, c'est qu'un
-                if not move:
-                    error("Erreur dans la reception du joueur {player_id}")
-                    exit()
+        for player_id, player in enumerate(self.players):
+            player.send(self.build_message("NEWTURN", [0]))
+            # Envoie de tous les nouveaux événements
+            move = player.recv(255).decode()
 
-                # On execute la commande envoyée par le joueur
-                command, *params = self.parse_message(move)
-                err, response = self.handle_command(player_id, command, params)
-                print(f"DEBUG:  {command=} ")
-                commandeJoueur[f"{player_id}"]=command
-                # Si une erreur a eu lieu, on quite directement le programme 
-                # (cela signifie que le code du client n'est pas bon)
-                if err:
-                    self.error(response)
+            # Si cela arrive, c'est qu'un
+            if not move:
+                self.error(f"Erreur dans la reception du joueur {player_id}")
+                exit()
+
+            # On execute la commande envoyée par le joueur
+            command, *params = self.parse_message(move)
+            err = self.handle_command(player_id, command, params)
+            commandeJoueur[player_id]=(command,params)
+            # Si une erreur a eu lieu, on quite directement le programme 
+            # (cela signifie que le code du client n'est pas bon)
+            if err:
+                self.error("ERROR: COMMANDE INTROUVABLE")
 
         return commandeJoueur
 
@@ -415,12 +432,8 @@ class Serveur():
         return (0, "")
 
     def handle_command(self,player_id, command, params):
-        commands = {'MOVE': "move"}
-        if not command in commands:
-            return (1, "Commande inexistante")
-
-        return commands[command](player_id, params)
-
+        commands = {'MOVE',"STAY"}
+        return not command in commands
 
 
 
